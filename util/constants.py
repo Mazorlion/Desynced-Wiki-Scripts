512063a48e3977d8c39970f73eeae23bbf93a281
Kelno <kelnowm@gmail.com>
2025-07-31 18:55:31 +0200

    Added some more renames & force includes

--- util/constants.py
+++ util/constants.py
@@ -21,5 +21,33 @@ FORCE_INCLUDE_NAMES = {
     "Greelobyte",
     "Trilopew",
     "Wasp",
+    "Gigakaiju",
     "Shield Worm",
+    "Bug Hole",
+    "Bug Hive",
+    "Large Bug Hive",
+    "Giant Beast",
+}
+
+# List of object for which to override names with a custom name
+# This is used to handle entries having the same names in the game, but need to be differentiated in the wiki.
+# Example: Both c_human_aicenter and c_mission_human_aicenter are called "AI Research Center" in the game.
+# Supports entities, items, components
+WIKI_NAME_OVERRIDES: dict[str, str] = {
+    "c_mission_human_aicenter": "AI Research Center (Mission)",
+    "f_bot_2m_as": "Command Center (Bot)",
+    "c_internal_crane2": "Item Transporter (Large Beacon)",
+    "c_internal_crane1": "Item Transporter (Beacon)",
+    "c_internal_transporter": "Item Transporter (Human Warehouse)",
+    "f_building2x1d": "Building 2x1 (1M) (Stockpile)",
+    "f_building2x1c": "Building 2x1 (2M) (Advanced Materials)",
+    "f_building2x2c": "Building 2x2 (2M1L) (Epic Structures) (A)",
+    "f_building2x2d": "Building 2x2 (2M1L) (Epic Structures) (B)",
+    "c_trilobyte_attack1": "Trilobyte Attack (1)",
+    "c_trilobyte_attack2": "Trilobyte Attack (2)",
+    "c_trilobyte_attack3": "Trilobyte Attack (3)",
+    "c_trilobyte_attack4": "Trilobyte Attack (4)",
+    "c_trilobyte_attack_t2": "Trilobyte Attack (T2)",
+    "c_trilobyte_attack_t3": "Trilobyte Attack (T3)",
+    "f_bug_home": "Bug Hole (Giant)",
 }
--- wiki/wiki_name_overrides.py
+++ wiki/wiki_name_overrides.py
@@ -1,30 +1,6 @@
-# List of object for which to override names with a custom name
-# This is used to handle entries having the same names in the game, but need to be differentiated in the wiki.
-
-# Example: Both c_human_aicenter and c_mission_human_aicenter are called "AI Research Center" in the game.
-
 from typing import Any
 
-from models.decorators import desynced_object
-
-# Supports entities, items, components
-WIKI_NAME_OVERRIDES: dict[str, str] = {
-    "c_mission_human_aicenter": "AI Research Center (Mission)",
-    "f_bot_2m_as": "Command Center (Bot)",
-    "c_internal_crane2": "Item Transporter (Large Beacon)",
-    "c_internal_crane1": "Item Transporter (Beacon)",
-    "c_internal_transporter": "Item Transporter (Human Warehouse)",
-    "f_building2x1d": "Building 2x1 (1M) (Stockpile)",
-    "f_building2x1c": "Building 2x1 (2M) (Advanced Materials)",
-    "f_building2x2c": "Building 2x2 (2M1L) (Epic Structures) (A)",
-    "f_building2x2d": "Building 2x2 (2M1L) (Epic Structures) (B)",
-    "c_trilobyte_attack1": "Trilobyte Attack (1)",
-    "c_trilobyte_attack2": "Trilobyte Attack (2)",
-    "c_trilobyte_attack3": "Trilobyte Attack (3)",
-    "c_trilobyte_attack4": "Trilobyte Attack (4)",
-    "c_trilobyte_attack_t2": "Trilobyte Attack (T2)",
-    "c_trilobyte_attack_t3": "Trilobyte Attack (T3)",
-}
+from util.constants import WIKI_NAME_OVERRIDES
 
 
 def get_name_override(lua_id: str) -> str | None:
